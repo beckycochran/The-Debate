4c3a16f82950afa228953c0d0a3be6e39dddf1f8
beckyparkes <rebekahmparkes@outlook.com>
2022-08-28 00:02:09 -0400

    global styles and basic components

---
+++ pages/body.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Body = () => {
+    return (
+        <Wrapper>
+            Body
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.div`
+  display: flex;
+`
+export default Body;
---
+++ pages/header.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Header = () => {
+    return (
+        <Wrapper>
+            Header
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.div`
+  display: flex;
+`
+export default Header;
