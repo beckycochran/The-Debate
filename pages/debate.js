1b1ffd9bb4a4b425dd25a2babb37c26d73d56a4a
beckyparkes <rebekahmparkes@outlook.com>
2022-08-31 18:34:34 -0700

    / path goes to /debate

---
+++ pages/debate.js
@@ -0,0 +1,15 @@
+import GlobalStyles from "./global-styles";
+import Header from "./header";
+import Body from "./body";
+
+const Debate = () => {
+  return (
+   <>
+    <GlobalStyles />
+    <Header />
+    <Body />
+   </>
+  )
+}
+
+export default Debate;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
 import GlobalStyles from "./global-styles";
-import Header from "./header";
-import Body from "./body";
+import Link from 'next/link';
+import styled from "styled-components"
 
 const Index = () => {
   return (
-   <>
-    <GlobalStyles />
-    <Header />
-    <Body />
-   </>
+    <>
+      <Wrapper>
+        <GlobalStyles />
+        <SLink href='/debate'>Click me to see the debate of the day</SLink>
+        <SLink href='/debate'>Archived Debates</SLink>
+      </Wrapper>
+    </>
   )
 }
+
+const Wrapper = styled.div`
+display: flex;
+height: 100vh;
+width: 100vw;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`
+const SLink = styled(Link)`  
+align-text: center;
+`
 
 export default Index;
