fa5e7572ddb9d0d551ece1f1f7e5c731d0dfdbac
beckyparkes <rebekahmparkes@outlook.com>
2022-11-12 20:26:45 -0500

    login button changed

--- src/header.js
+++ src/header.js
@@ -1,23 +1,31 @@
-import styled from "styled-components"
+import styled from "styled-components";
+
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+
 const Header = () => {
     return (
-        <Wrapper>
-            <LB src="/login-button.svg" alt="Login" />
-        </Wrapper>
+        <LoginButton>
+            Login
+        </LoginButton>
     )
 }
 
 export default Header;
 
-const Wrapper = styled.div`
-    background: none;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    padding-right: 1%;
-`
-const LB = styled.img`
-    color:white;
-    max-height:5vh;
-    max-width:5vw;
+const LoginButton = styled.button`
+width:fit-content;
+border:none;
+float:right;
+padding-right: 1%;
+padding-top: 0.5%;
+z-index:-1;
+background-color:inherit;
+color: white;
+font-size: 3vh;
+
+&:hover {
+    font-weight: bold;
+    opacity: 0.8;
+    cursor: pointer;
+}
 `
