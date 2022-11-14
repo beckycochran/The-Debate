30f32c452c1856d9209274d7bfee6b11d669fc1b
beckyparkes <rebekahmparkes@outlook.com>
2022-11-14 13:48:18 -0500

    configuring auth0, added auth directory under pages/api

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import '../styles/globals.css'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+// import { config } from '@fortawesome/fontawesome-svg-core'
+// import '@fortawesome/fontawesome-svg-core/styles.css'
+// config.autoAddCss = false
+
+import { Auth0Provider } from '@auth0/auth0-react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // <Auth0Provider
+    //   domain={ }
+    //   clientId={ }
+    //   redirectUri={'http://localhost/3000/debate/main'}>
+      <Component {...pageProps} />
+    // </Auth0Provider> 
+  )
 }
 
 export default MyApp
