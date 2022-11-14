463f5fa9a89e0c89ffbb281e44edbdd79b4e5cc7
beckyparkes <rebekahmparkes@outlook.com>
2022-11-14 14:29:43 -0500

    remove all instances of fontawesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import '../styles/globals.css'
-// import { config } from '@fortawesome/fontawesome-svg-core'
-// import '@fortawesome/fontawesome-svg-core/styles.css'
-// config.autoAddCss = false
+
 
 import React from 'react'
 import { UserProvider } from '@auth0/nextjs-auth0'
