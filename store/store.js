c2113bf3e1594e56094501c2b0ca792d2a7a8cc8
beckyparkes <rebekahmparkes@outlook.com>
2022-11-17 15:22:44 -0500

    rework store.js with configureStore from redux/toolkit

--- store/store.js
+++ store/store.js
@@ -1,23 +1,10 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { createWrapper } from "next-redux-wrapper";
-import rootReducer from "./reducers";
+import { configureStore } from "@reduxjs/toolkit";
+import orderReducer from "../slices/orderSlice";
 
-// initial states here
-const initalState = {};
-
-// middleware
-const middleware = [thunk];
-
-// creating store
-export const store = createStore(
-  rootReducer,
-  initalState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
-
-// assigning store to next wrapper
-const makeStore = () => store;
-
-export const wrapper = createWrapper(makeStore);
+//Global store
+export const store = configureStore({
+  reducer: {
+    //reducers are defined here
+    order: orderReducer,
+  },
+});
