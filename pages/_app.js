f451b0be83f6416aaee9303b5c789357414bc089
beckyparkes <rebekahmparkes@outlook.com>
2022-11-17 15:13:37 -0500

    wrap react-redux Provider component around app, make store available as a global prop

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
 import '../styles/globals.css'
 import React from 'react'
 import { UserProvider } from '@auth0/nextjs-auth0'
+import {wrapper, store} from "../store/store"
+import { Provider } from "react-redux"
 
 
 function MyApp({ Component, pageProps }) {
   return (
+    <Provider store={store}>
     <UserProvider>
       <Component {...pageProps} />
     </UserProvider>
+    </Provider>
   )
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp)
