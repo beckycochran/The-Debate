923bfbfe6c2d3d88e2e2bf0f66dfb2baf02ce9d4
beckyparkes <rebekahmparkes@outlook.com>
2022-12-22 12:33:49 -0500

    pseudo code for discussion page

--- pages/discussion/[id].js
+++ pages/discussion/[id].js
@@ -5,7 +5,15 @@ const Discussion = () => {
     const { id } = router.query
 
     return (
+        <>
             <p>Discussion side: {id}</p>
+            {/* Discussion body */}
+            {/* Options to add to discussion */}
+            {/* Comments can be promoted or elected for addition to main page */}
+            {/* When a comment passes a certain qualification, it is put into the '/debate' page on the appropriate side */}
+            {/* Selecting the comment in '/debate' sends the user back to its location here */}
+        </>
+
     )
 }
 
